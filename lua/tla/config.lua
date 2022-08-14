600623c76676f561878bbf1536b5ac2a38191ee2
zerosign <r1nlx0@gmail.com>
2022-08-14 15:22:55 +0700

    chore: change defaults configuration paths

--- lua/tla/config.lua
+++ lua/tla/config.lua
@@ -2,9 +2,9 @@ local Path = require("plenary.path")
 local utils = require("tla.utils")
 
 local config = {
-	java_executable = Path:new(vim.fn.getenv("JAVA_HOME"), "bin", "java").filename,
+	java_executable = "/usr/bin/java",
 	java_opts = { "-XX:+UseParallelGC" },
-	tla2tools = Path:new(utils.tla_nvim_cache_dir, "tla2tools.jar").filename,
+	tla2tools = "~/.local/share/tlaplus/tla2tools.jar",
 }
 
 return config
